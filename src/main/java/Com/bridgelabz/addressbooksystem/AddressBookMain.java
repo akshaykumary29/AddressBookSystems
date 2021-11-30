38566a33be597996838ceed17a669a6a19a3f235
akshay <akshay.yamgar29@gmail.com>
2021-11-30 08:30:01 +0530

    Added: Welcome message added

---
+++ src/main/java/Com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -0,0 +1,8 @@
+package Com.bridgelabz.addressbooksystem;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welome To Address Book Program !!!");
+    }
+
+}
